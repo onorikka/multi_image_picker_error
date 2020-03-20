2fb5d142a9b39733e6a6c18bf8fb71cf98623bcd
onorikka <116250767+onorikka@users.noreply.github.com>
2020-03-20 06:39:01 +0000

    initial set up

--- evaporate.js
+++ evaporate.js
@@ -24,3 +24,71 @@
 
   var FAR_FUTURE = new Date('2060-10-22'),
       HOURS_AGO,
+      PENDING = 0, EVAPORATING = 2, COMPLETE = 3, PAUSED = 4, CANCELED = 5, ERROR = 10, ABORTED = 20, PAUSING = 30,
+      PAUSED_STATUSES = [PAUSED, PAUSING],
+      ACTIVE_STATUSES = [PENDING, EVAPORATING, ERROR],
+      ETAG_OF_0_LENGTH_BLOB = '"d41d8cd98f00b204e9800998ecf8427e"',
+      PARTS_MONITOR_INTERVAL_MS = 2 * 60 * 1000,
+      IMMUTABLE_OPTIONS = [
+        'maxConcurrentParts',
+        'logging',
+        'cloudfront',
+        'encodeFilename',
+        'computeContentMd5',
+        'allowS3ExistenceOptimization',
+        'onlyRetryForSameFileName',
+        'timeUrl',
+        'cryptoMd5Method',
+        'cryptoHexEncodedHash256',
+        'awsRegion',
+        'awsSignatureVersion',
+        'evaporateChanged'
+      ],
+      S3_EXTRA_ENCODED_CHARS =  {
+        33: "%21", // !
+        39: "%27", // '
+        40: "%28", // (
+        41: "%29", // )
+        42: "%2A"  // *
+      },
+      l;
+
+  var Evaporate = function (config) {
+    this.config = extend({
+      readableStreams: false,
+      readableStreamPartMethod: null,
+      bucket: null,
+      logging: true,
+      maxConcurrentParts: 5,
+      partSize: 6 * 1024 * 1024,
+      retryBackoffPower: 2,
+      maxRetryBackoffSecs: 300,
+      progressIntervalMS: 1000,
+      cloudfront: false,
+      s3Acceleration: false,
+      mockLocalStorage: false,
+      encodeFilename: true,
+      computeContentMd5: false,
+      allowS3ExistenceOptimization: false,
+      onlyRetryForSameFileName: false,
+      timeUrl: null,
+      cryptoMd5Method: null,
+      cryptoHexEncodedHash256: null,
+      aws_key: null,
+      awsRegion: 'us-east-1',
+      awsSignatureVersion: '4',
+      sendCanonicalRequestToSignerUrl: false,
+      s3FileCacheHoursAgo: null, // Must be a whole number of hours. Will be interpreted as negative (hours in the past).
+      signParams: {},
+      signHeaders: {},
+      customAuthMethod: undefined,
+      maxFileSize: null,
+      signResponseHandler: null,
+      xhrWithCredentials: false,
+      // undocumented, experimental
+      localTimeOffset: undefined,
+      evaporateChanged: function () {},
+      abortCompletionThrottlingMs: 1000
+    }, config);
+
+    if (typeof window !== 'undefined' && window.console) {
