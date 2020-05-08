c062c45a38a677e767b884eca6ab5beb51d0d813
onorikka <116250767+onorikka@users.noreply.github.com>
2020-05-08 16:44:25 +0000

    version 0.2.3

--- example/SignerV4Example.java
+++ example/SignerV4Example.java
@@ -3,4 +3,6 @@ package example;
 import java.io.IOException;
 
 import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpe
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.ServletException;
+import javax.ser
