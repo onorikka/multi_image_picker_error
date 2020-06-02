a40daf471db7da63d4a25483692441cdf8ddfe8f
onorikka <116250767+onorikka@users.noreply.github.com>
2020-06-02 10:39:19 +0000

    more pics

--- example/SignerV4Example.java
+++ example/SignerV4Example.java
@@ -49,4 +49,8 @@ public class SignerV4Example extends HttpServlet {
 		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws S
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	static byt
