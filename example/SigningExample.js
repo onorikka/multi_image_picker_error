909adc8dd35fec07dc6ed6be360d21a20040c20f
onorikka <116250767+onorikka@users.noreply.github.com>
2020-06-22 21:51:59 +0000

    document updated

--- example/SigningExample.js
+++ example/SigningExample.js
@@ -2,4 +2,7 @@
 //npm install express body-parser and navigate to http://127.0.0.1:8080/index.html
 var express = require('express');
 var bodyParser = require('body-parser');
-var c
+var crypto = require('crypto');
+
+var app = express();
+app.use(express.static
