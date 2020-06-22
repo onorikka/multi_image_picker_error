cc12d6760f5b18ba4f233069e5256ae08443203f
onorikka <116250767+onorikka@users.noreply.github.com>
2020-06-22 19:39:49 +0000

    usb: wusbcore: change WA_SEGS_MAX to a legal value

--- example/SigningExample.js
+++ example/SigningExample.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 //npm install express body-parser and navigate to http://127.0.0.1:8080/index.html
-var express = require('ex
+var express = require('express');
+var bodyParser = require('body-parser');
+var c
