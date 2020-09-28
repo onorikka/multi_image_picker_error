7ef2da90a680833f7506a4df11a3b49bc0ad9e78
onorikka <116250767+onorikka@users.noreply.github.com>
2020-09-28 10:09:29 +0000

    Обновление документации

--- example/electron/main.js
+++ example/electron/main.js
@@ -2,4 +2,6 @@ const electron = require('electron')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
-const BrowserWindow = 
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path'
