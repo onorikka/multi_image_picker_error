e472b94fe6b48600a803836dcf552f7afe4ef0a3
onorikka <116250767+onorikka@users.noreply.github.com>
2020-09-28 17:10:48 +0000

    Removed unnecessary includes

--- example/electron/main.js
+++ example/electron/main.js
@@ -4,4 +4,7 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path'
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of th
