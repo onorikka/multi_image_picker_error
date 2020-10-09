f9818d853a81afccae1b4d4f7a59eb1de1153d5d
onorikka <116250767+onorikka@users.noreply.github.com>
2020-10-09 17:43:06 +0000

    llx

--- example/electron/main.js
+++ example/electron/main.js
@@ -13,4 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  mainWindow.lo
