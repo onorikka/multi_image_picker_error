f599b79fe3cd108d51e2ef9d99a449aceac74321
onorikka <116250767+onorikka@users.noreply.github.com>
2020-10-13 10:00:03 +0000

    comments only

--- example/electron/main.js
+++ example/electron/main.js
@@ -28,4 +28,5 @@ function createWindow () {
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
-    // in an array if your app s
+    // in an array if your app supports multi windows, this is the time
+    // when you should 
