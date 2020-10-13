469012298dc5d9fdfcd6a5654aed6d5e95cec555
onorikka <116250767+onorikka@users.noreply.github.com>
2020-10-13 11:03:10 +0000

    chore(package): update mocha to version 3.5.1

--- example/electron/main.js
+++ example/electron/main.js
@@ -29,4 +29,5 @@ function createWindow () {
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
-    // when you should 
+    // when you should delete the corresponding element.
+    mainWindow = null
