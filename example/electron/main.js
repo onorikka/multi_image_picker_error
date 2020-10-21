7236f14096b11b5ae6ae1873d299ed429eee838a
onorikka <116250767+onorikka@users.noreply.github.com>
2020-10-21 12:42:51 +0000

    increase version number

--- example/electron/main.js
+++ example/electron/main.js
@@ -37,4 +37,6 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.o
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
