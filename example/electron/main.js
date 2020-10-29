31c43615c60414df518ed293b17527fc7d41c65c
onorikka <116250767+onorikka@users.noreply.github.com>
2020-10-29 12:24:51 +0000

    bump pod version

--- example/electron/main.js
+++ example/electron/main.js
@@ -50,4 +50,5 @@ app.on('window-all-closed', function () {
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
-  // dock icon is cl
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow ==
