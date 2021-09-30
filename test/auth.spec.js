c771c8bc018313ff0f4fda4c579c49426b8fcf89
onorikka <116250767+onorikka@users.noreply.github.com>
2021-09-30 23:08:43 +0000

    New translations ja.yml (French)

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -16,3 +16,51 @@ const customAuthHandler = function  () {
 }
 
 function testCommonAuthorization(t, addCfg, evapConfig) {
+  const addConfig = Object.assign({}, t.context.baseAddConfig, addCfg, {
+    error: function (msg) {
+      t.context.errMessages.push(msg);
+    }})
+
+  evapConfig = Object.assign({ s3FileCacheHoursAgo: 24 }, evapConfig)
+  return testBase(t, addConfig, evapConfig);
+}
+
+function v2Authorization(signature) {
+  return 'AWS testkey:' + signature
+}
+function testV2Authorization(t, initConfig, addCfg) {
+  const config = {awsSignatureVersion: '2', signerUrl: 'http://what.ever/signv2'}
+  const evapConfig = Object.assign({}, config, initConfig)
+
+  return testCommonAuthorization(t, addCfg, evapConfig);
+}
+function testV2ToSign(t, request, amzHeaders, addConfig, evapConfig) {
+  return testV2Authorization(t, evapConfig, addConfig)
+      .then(function () {
+        var qp = params(testRequests[t.context.testId][2].url),
+            h = Object.assign({}, amzHeaders, {testId: t.context.testId, 'x-amz-date': qp.datetime}),
+            r = Object.assign({}, request, {x_amz_headers: h, contentType: (addConfig ? addConfig.contentType : undefined)}),
+            expected = encodeURIComponent(stringToSignV2('/' + AWS_BUCKET + '/' + t.context.config.name +
+                '?partNumber=1&uploadId=Hzr2sK034dOrV4gMsYK.MMrtWIS8JVBPKgeQ.LWd6H8V2PsLecsBqoA1cG1hjD3G4KRX_EBEwxWWDu8lNKezeA--', 'PUT', r))
+
+        return new Promise(function (resolve, reject) {
+          var result = {
+            result: qp.to_sign,
+            expected: expected
+          }
+          resolve(result)
+
+        })
+      })
+
+}
+function testV2ListParts(t, request, amzHeaders, addConfig, maxGetParts, partNumberMarker, evapConfig) {
+  t.context.partNumberMarker = partNumberMarker
+  t.context.maxGetParts = maxGetParts
+
+  addConfig = Object.assign({}, {file: new File({
+        path: '/tmp/file',
+        size: 29690176,
+        name: 'tests'
+      })}, addConfig)
+  return testV2Authorization(t, evapConfig, addConfig)
