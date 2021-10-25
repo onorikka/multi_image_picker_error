ce9f390c605652e5442f6640d4989da63a9e6c52
onorikka <116250767+onorikka@users.noreply.github.com>
2021-10-25 18:39:34 +0000

    libceph: WARN, don't BUG on unexpected connection states

--- test/cancel-abort.spec.js
+++ test/cancel-abort.spec.js
@@ -25,3 +25,53 @@ function testCommon(t, addConfig, initConfig) {
 
 function testCancel(t, addConfig) {
   addConfig = addConfig || {}
+
+  const config = Object.assign({}, {
+    started: sinon.spy(),
+    cancelled: sinon.spy()
+  },
+      addConfig)
+
+  return testCommon(t, config)
+      .then(function () {
+        t.context.cancelPromise = t.context.cancel()
+        return t.context.cancelPromise
+      })
+}
+
+test.before(() => {
+  sinon.xhr.supportsCORS = true
+  global.XMLHttpRequest = sinon.useFakeXMLHttpRequest()
+  global.window = {
+    localStorage: {},
+    console: console
+  };
+
+  function partRequestHandler(xhr, context)  {
+    if (context.pauseUpload) {
+      if (xhr.url.indexOf('partNumber=1') > -1) {
+          context.pause(context.forcePause)
+              .then(context.pausedPromise.resolve)
+      }
+    }
+    return true
+  }
+
+  server = serverCommonCase(partRequestHandler)
+})
+
+test.beforeEach((t) => {
+  beforeEachSetup(t)
+
+  t.context.cancel = function () {
+    return t.context.evaporate.cancel(t.context.uploadId)
+  }
+  t.context.pause = function (force) {
+    return t.context.evaporate.pause(t.context.uploadId, {force: force})
+  }
+})
+
+// Default Setup: V2 signatures, Cancel
+test('should Cancel an upload calling started once', (t) => {
+  return testCancel(t)
+      .then(function () {
