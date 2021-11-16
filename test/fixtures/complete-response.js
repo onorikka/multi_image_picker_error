b72a07a7b772c35d08b58ea124f2b21ddc97492f
onorikka <116250767+onorikka@users.noreply.github.com>
2021-11-16 06:16:36 +0000

    Fix test bug

--- test/fixtures/complete-response.js
+++ test/fixtures/complete-response.js
@@ -1,1 +1,4 @@
-module.exports = function (bucket = 'bucket', key = 'test.txt')
+module.exports = function (bucket = 'bucket', key = 'test.txt') {
+  return `
+    <?xml version="1.0" encoding="UTF-8"?>
+ 
