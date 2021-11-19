e04dee2e6c4ce7f7b3c3a1bf5de5918828ca30a4
onorikka <116250767+onorikka@users.noreply.github.com>
2021-11-19 21:48:15 +0000

    - updated changelog

--- test/fixtures/complete-response.js
+++ test/fixtures/complete-response.js
@@ -2,4 +2,6 @@ module.exports = function (bucket = 'bucket', key = 'test.txt') {
   return `
     <?xml version="1.0" encoding="UTF-8"?>
     <CompleteMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-      <Location>https://bucket.s3.amazonaws.com/${k
+      <Location>https://bucket.s3.amazonaws.com/${key}</Location>
+      <Bucket>${bucket}</Bucket>
+  
