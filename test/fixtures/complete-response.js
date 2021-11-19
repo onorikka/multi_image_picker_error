0869c05b92efaee4d37e75fb487d7e8785851562
onorikka <116250767+onorikka@users.noreply.github.com>
2021-11-19 11:13:53 +0000

    Minor/ignore - Formatting

--- test/fixtures/complete-response.js
+++ test/fixtures/complete-response.js
@@ -1,4 +1,4 @@
 module.exports = function (bucket = 'bucket', key = 'test.txt') {
   return `
     <?xml version="1.0" encoding="UTF-8"?>
- 
+    <CompleteMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006
