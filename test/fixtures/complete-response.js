26c2b4066fcadca021425be8034c8c188654a177
onorikka <116250767+onorikka@users.noreply.github.com>
2021-11-19 18:08:19 +0000

    [maven-release-plugin] prepare release 1.1.1

--- test/fixtures/complete-response.js
+++ test/fixtures/complete-response.js
@@ -1,4 +1,5 @@
 module.exports = function (bucket = 'bucket', key = 'test.txt') {
   return `
     <?xml version="1.0" encoding="UTF-8"?>
-    <CompleteMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006
+    <CompleteMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+      <Location>https://bucket.s3.amazonaws.com/${k
