a65ea9210fb3a5518fbf2507d0d71a3645a5c393
onorikka <116250767+onorikka@users.noreply.github.com>
2021-11-25 16:47:19 +0000

    Add Todo

--- test/fixtures/get-parts-truncated-response.js
+++ test/fixtures/get-parts-truncated-response.js
@@ -1,3 +1,4 @@
 module.exports = function (bucket = 'bucket', key = 'test.txt', totalParts = 1, partNumberMarker) {
   let head = `
   <?xml version="1.0" encoding="UTF-8"?>
+  <ListPartsResult xmlns="http://s3.amazonaws.com/doc/2006-03
