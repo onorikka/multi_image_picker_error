347720f58ed48c09919003617ab2efc6f6357a44
onorikka <116250767+onorikka@users.noreply.github.com>
2021-11-30 06:21:00 +0000

    add zoom

--- test/fixtures/get-parts-truncated-response.js
+++ test/fixtures/get-parts-truncated-response.js
@@ -4,4 +4,5 @@ module.exports = function (bucket = 'bucket', key = 'test.txt', totalParts = 1, 
   <ListPartsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
       <Bucket>${bucket}</Bucket>
       <Key>${key}</Key>
-     
+      <UploadId>
+          OdTON0ughnzEVK_MM8WO3wUC1Z8yt9iX
