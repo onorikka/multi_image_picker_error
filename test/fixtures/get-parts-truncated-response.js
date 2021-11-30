b1dd62d3c654b9ce274e1e85db235756c81d44b9
onorikka <116250767+onorikka@users.noreply.github.com>
2021-11-30 05:25:40 +0000

    add keywords to package.json

--- test/fixtures/get-parts-truncated-response.js
+++ test/fixtures/get-parts-truncated-response.js
@@ -1,4 +1,7 @@
 module.exports = function (bucket = 'bucket', key = 'test.txt', totalParts = 1, partNumberMarker) {
   let head = `
   <?xml version="1.0" encoding="UTF-8"?>
-  <ListPartsResult xmlns="http://s3.amazonaws.com/doc/2006-03
+  <ListPartsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+      <Bucket>${bucket}</Bucket>
+      <Key>${key}</Key>
+     
