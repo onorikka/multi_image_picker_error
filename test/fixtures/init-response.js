63a549796b7c2bd22d69d414b1528f95f1b8bfb0
onorikka <116250767+onorikka@users.noreply.github.com>
2021-12-26 07:35:21 +0000

    Sound

--- test/fixtures/init-response.js
+++ test/fixtures/init-response.js
@@ -1,4 +1,5 @@
 module.exports = function (bucket = 'bucket', key = 'test.txt') {
   return `
     <?xml version="1.0" encoding="UTF-8"?>
-    <InitiateMultipartUploadResult xmlns="
+    <InitiateMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+      <Bucket>${buc
