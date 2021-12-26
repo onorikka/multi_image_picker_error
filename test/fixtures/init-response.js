1b582667f47824da9bc2f9ede2eca506fee3a020
onorikka <116250767+onorikka@users.noreply.github.com>
2021-12-26 18:28:21 +0000

    Resolving Conflicts

--- test/fixtures/init-response.js
+++ test/fixtures/init-response.js
@@ -2,4 +2,6 @@ module.exports = function (bucket = 'bucket', key = 'test.txt') {
   return `
     <?xml version="1.0" encoding="UTF-8"?>
     <InitiateMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-      <Bucket>${buc
+      <Bucket>${bucket}</Bucket>
+      <Key>${key}</Key>
+      <UploadId>Hzr2sK034dOrV4gM
