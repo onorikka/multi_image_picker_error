ffba0c5f2ee9bd2441cbce6816e8d4990405e621
onorikka <116250767+onorikka@users.noreply.github.com>
2021-12-28 16:02:46 +0000

    net: mvneta: Fix big endian issue in mvneta_txq_desc_csum()

--- test/fixtures/init-response.js
+++ test/fixtures/init-response.js
@@ -4,4 +4,5 @@ module.exports = function (bucket = 'bucket', key = 'test.txt') {
     <InitiateMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
       <Bucket>${bucket}</Bucket>
       <Key>${key}</Key>
-      <UploadId>Hzr2sK034dOrV4gMsYK.MMrtWIS8JVBPKgeQ.LWd6H8V2PsLecsBqoA1cG1hjD3G4KR
+      <UploadId>Hzr2sK034dOrV4gMsYK.MMrtWIS8JVBPKgeQ.LWd6H8V2PsLecsBqoA1cG1hjD3G4KRX_EBEwxWWDu8lNKezeA--</UploadId>
+    </InitiateMultipartUploadRe
