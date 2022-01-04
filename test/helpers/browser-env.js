2a5ba5239a8ab4c9b018cd2d38c331d7cd2b3c97
onorikka <116250767+onorikka@users.noreply.github.com>
2022-01-04 07:25:32 +0000

    enchance

--- test/helpers/browser-env.js
+++ test/helpers/browser-env.js
@@ -4,4 +4,5 @@ import { File } from 'file-api'
 import Evaporate from '../../evaporate'
 import sinon from 'sinon'
 import initResponse from '../fixtures/init-response'
-import completeResponse from '../fixtures/complete-respo
+import completeResponse from '../fixtures/complete-response'
+import getPartsResponse from '../fixtures/get-par
