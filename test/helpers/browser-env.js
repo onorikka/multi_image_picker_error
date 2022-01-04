c2f0c953d3a556b67e90d3bf409a5f32f4f0d196
onorikka <116250767+onorikka@users.noreply.github.com>
2022-01-04 18:38:04 +0000

    Create NOTES.md

--- test/helpers/browser-env.js
+++ test/helpers/browser-env.js
@@ -8,4 +8,6 @@ import completeResponse from '../fixtures/complete-response'
 import getPartsResponse from '../fixtures/get-parts-truncated-response'
 
 const CONTENT_TYPE_XML = { 'Content-Type': 'text/xml' }
-const CONTENT_TYPE_TEXT = { 'Cont
+const CONTENT_TYPE_TEXT = { 'Content-Type': 'text/plain' }
+
+const AWS_UPLOAD_
