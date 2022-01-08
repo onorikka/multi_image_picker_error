5f90b714ace2b94bd10e0a89ad9d30d58b060030
onorikka <116250767+onorikka@users.noreply.github.com>
2022-01-08 13:14:19 +0000

    fixed some problems

--- test/helpers/browser-env.js
+++ test/helpers/browser-env.js
@@ -10,4 +10,7 @@ import getPartsResponse from '../fixtures/get-parts-truncated-response'
 const CONTENT_TYPE_XML = { 'Content-Type': 'text/xml' }
 const CONTENT_TYPE_TEXT = { 'Content-Type': 'text/plain' }
 
-const AWS_UPLOAD_
+const AWS_UPLOAD_KEY = 'tests'
+
+global.document = jsdom('<body></body>')
+globa
